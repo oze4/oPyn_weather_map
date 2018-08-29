caf0c3fd15614dc0472e1fee0373a63d892831d6
moestreich <moestreich@centretechnologies.com>
2018-08-29 16:33:24 -0500

    update

--- Summary.py
+++ Summary.py
@@ -1,13 +1,22 @@
-import CityCoordinate
-import Clouds
-import DescriptionInfo
-import Measurements
-import Sys
-import Wind
+from CityCoordinate import *
+from Clouds import *
+from DescriptionInfo import *
+from Measurements import *
+from Sys import *
+from Wind import *
 
 
-class Summary(CityCoordinate, Clouds, DescriptionInfo, Measurements, Sys, Wind):
-    def __init__(self, timestamp, weather_id, city, code, visibility, base_, date_time):
+class Summary(object):
+
+    def __init__(self, timestamp, weather_id, city, code, visibility, base_, date_time,
+                 wind
+                 ):
+        # ~base params timestamp, weather_id, city, code, visibility, base_, date_time, ~params from 'Clouds' object
+        # clouds_all, ~params from 'Wind' object wind_speed, ~params from 'Sys' object sys_type, sys_id, sys_message,
+        #  sys_country, sys_sunrise, sys_sunset, ~params from 'Measurements' object measurement_temp,
+        # measurement_pressure, measurement_humidity, measurement_temp_min, measurement_temp_max, ~params from
+        # 'DescriptionInfo' object dscptn_info_id_, dscptn_info_main_, dscptn_info_description, dscptn_info_icon,
+        # ~params from 'CityCoordinate' object city_coord_longitude, city_coord_latitude ):
         self.Timestamp = timestamp
         self.Weather_Id = weather_id
         self.City = city
@@ -15,3 +24,4 @@ class Summary(CityCoordinate, Clouds, DescriptionInfo, Measurements, Sys, Wind):
         self.Visibility = visibility
         self.Base_ = base_
         self.Date_Time = date_time
+        self.Wind.Speed = wind.Speed
